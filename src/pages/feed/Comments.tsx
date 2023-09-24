162d290b5f6a8a8b925eda10129ef43b413040df
Eden Chan <edenchan717@gmail.com>
2023-09-24 08:21:52 -0400

    feat: add emoji slider and center feed

--- src/pages/feed/Comments.tsx
+++ src/pages/feed/Comments.tsx
@@ -1,32 +1,17 @@
 'use client'
-import React, { useMemo, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState } from "react";
 import { ProfileId, PublicationId, useActiveProfile, useComments } from "@lens-protocol/react-web";
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-import { Slider } from "@/components/ui/slider";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue, } from "@/components/ui/select"
 import { Button } from "@/components/ui/button";
-import { useQRCode } from "next-qrcode";
-import useGenerateQrCode from "@/hooks/useGenerateQrCode";
-import useCheckForResponse from "@/hooks/useVerificationResponse";
 import QRCode from "@/components/ui/QRCode";
+import EmojiSlider from "@/components/ui/emoji-slider";
 
+// Fetch the QR code from the server with loading + error states thanks to TanStack Query.
 
-
-  // Fetch the QR code from the server with loading + error states thanks to TanStack Query.
-
-
-  const IDENTITIES = ['Physics', 'Chemistry', 'Biology']
-const Comments = ({ postId } : { postId : PublicationId}) => {
+const IDENTITIES = ['Physics', 'Chemistry', 'Biology']
+const Comments = ({ postId }: { postId: PublicationId }) => {
   // Generate a unique session ID using uuid library.
-    const {data: commentObjects, loading: isCommentsLoading, hasMore, next} = useComments({commentsOf: postId});
+  const { data: commentObjects, loading: isCommentsLoading, hasMore, next } = useComments({ commentsOf: postId });
   const { data: activeProfile, loading: isLoginLoading } = useActiveProfile();
   // Used to render the QR code.
 
@@ -34,59 +19,59 @@ const Comments = ({ postId } : { postId : PublicationId}) => {
   const [selectedIdentity, setSelectedIdentity] = useState<string>("")
   const [isShowingQRCode, setIsShowingQRCode] = useState(false)
   const handleAddVote = () => {
-    setIsShowingQRCode(true) 
+    setIsShowingQRCode(true)
 
   }
 
-  const handleChangeIdentity = (value : string) => {
+  const handleChangeIdentity = (value: string) => {
     setIsShowingQRCode(false)
     setSelectedIdentity(value)
   }
 
+  const comments = commentObjects ? commentObjects.map(({ metadata: { content } }) => content) : [];
+  return (
+    <div className=" py-4 m-4 shadow-lg w-[180px]">
+      <Select defaultValue="comments">
+        <SelectTrigger className="w-[180px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="voting">Wiki3</SelectItem>
+            <SelectItem value="comments">{`Comments (${comments.length})`}</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <div className={'w-[180px]'}>
+        <label>Vibe Check</label>
+        <EmojiSlider />
+        {/*<Slider defaultValue={[50]} max={100} step={1} />*/}
+      </div>
+      <Select defaultValue="identity"
+              onValueChange={(value) => handleChangeIdentity(value)}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="identity">Select your role?</SelectItem>
+            {IDENTITIES.map(identity => (<SelectItem value={identity} key={identity}>{identity}</SelectItem>))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
 
-    const comments = commentObjects ? commentObjects.map(({ metadata: { content }}) => content ) : [];
-    return (
-        <div className="text-center overflow-y-scroll px-8 py-4 m-8 shadow-lg w-full">
-          <Select defaultValue="comments">
-            <SelectTrigger className="w-[180px]">
-            <SelectValue/>
-            </SelectTrigger>
-            <SelectContent>
-            <SelectGroup>
-                <SelectItem value="voting">Wiki3</SelectItem>
-                <SelectItem value="addComment">Add Comment</SelectItem>
-                <SelectItem value="comments">{`Comments (${comments.length})`}</SelectItem>
-            </SelectGroup>
-            </SelectContent>
-        </Select>
-        <label>Veracity Measure</label>
-          <Slider defaultValue={[50]} max={100} step={1} />
-            <Select value={selectedIdentity} onValueChange={(value) => handleChangeIdentity(value)}>
-            <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select your identity"/>
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    {IDENTITIES.map(identity=>(<SelectItem value={identity} key={identity}>{identity}</SelectItem>))}
-                </SelectGroup>
-            </SelectContent>
-            </Select>
+      <Button disabled={selectedIdentity === ""} onClick={handleAddVote}> Add Vote </Button>
+      {isShowingQRCode && <QRCode identity={selectedIdentity} />
 
-            <Button disabled={selectedIdentity === ""} onClick={handleAddVote}> Add Vote </Button>
-            {isShowingQRCode && <QRCode identity={selectedIdentity} />
-           
-          }
-          <div>
+      }
+      {
+        isCommentsLoading ?
+          <p> Loading ...</p> :
+          commentObjects?.map(({ id, metadata: { content } }) => <p key={id}>{content}</p>)
+      }
 
-          </div>
-            {
-                isCommentsLoading ? 
-                    <p> Loading ...</p> : 
-                    commentObjects?.map(({id, metadata: { content }}) => <p key={id}>{ content }</p>)
-            }
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Comments;
